b26d5c8c731aebb2f861aa6ab3ca21e1f162634c
tian <tianwu@outlook.com>
2022-07-11 09:49:25 +1000

    show ingredients

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -44,6 +44,18 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
             ),
+            // TODO: add Expanded
+            Expanded(
+                child: ListView.builder(
+                    padding: const EdgeInsets.all(7),
+                    itemCount: widget.recipe.ingredients.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final ingredient = widget.recipe.ingredients[index];
+                      // TODO: add ingredient.quantity
+                      return Text(
+                          '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                    })),
+            // TODO: add Slider()
           ],
         ),
       ),
